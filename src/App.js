d4b78d049ebb81ec5e5a37a64b336f438c665d82
Kristiyan Petrov <kristiqnpetrov7ii@gmail.com>
2024-02-09 09:18:40 +0100

    Task Tracker app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Go to the party",
+      day: "Feb 6th at 10pm",
+      reminder: true,
+    },
+  ]);
+
+  //Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  //Toggke reminder
+  const toggleReminder = (id) => {};
+  // const name = 'Brad'
+  // const x = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
+
+      {/* <h2>Hello {name}</h2>
+      <h3>Hello {x ? 'yes' : 'no'}</h3> */}
     </div>
   );
 }
 
 export default App;
+
+//import class
+
+// class Class extends React.Component{
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
+// export default App
